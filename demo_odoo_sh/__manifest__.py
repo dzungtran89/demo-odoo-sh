ae01f470ae17c85365dadf3109094ab1dd95394b
Dũng (Trần Đình) <dungtd@trobz.com>
2022-01-20 19:35:00 +0700

    [ADD] Init sh demo

---
+++ demo_odoo_sh/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Odoo SH Demo',
+    'version': '15.0.1.0',
+    'category': '',
+    'sequence': 5,
+    'summary': 'Odoo sh demo sale',
+    'description': """
+    Odoo sh demo sale
+    """,
+    'website': 'trobz.com',
+    'depends': ['sale_management'],
+    'data': [
+        'views/sale_order_view.xml'
+    ],
+    'demo': [
+    ],
+    'application': True,
+    'license': 'LGPL-3',
+}
